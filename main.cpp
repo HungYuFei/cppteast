93786633613aac0b2c70540bfca1fb93a5790fe1
HungYuFei <164743974+HungYuFei@users.noreply.github.com>
2025-10-16 14:51:32 +0800

    HW5

--- main.cpp
+++ main.cpp
@@ -2,10 +2,29 @@
 using namespace std;
 
 int main() {
-    for (int i = 1; i <= 50; i++) {
-        if ((i % 7) == 0) {  // 如果能被7整除
-            cout << i << " "; // 輸出該數字
+    // 顯示星號三角形
+    cout << "Star Pattern: \n";
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
         }
+        cout << endl;
     }
+    
+    cout << "\nMultiplication Table: \n";
+    
+    // 顯示九九乘法表
+    int i = 1;
+    while (i <= 9) {
+        cout << i;
+        int j = 1;
+        while (j <= 9) {
+            cout << " " << j << " = " << i * j << "\t";
+            j += 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+
     return 0;
 }
